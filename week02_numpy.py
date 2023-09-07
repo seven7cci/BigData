ff5aca727a82ccf88ae1b44995f845d691b6a9f6
seven7cci <seven7cci@naver.com>
2023-09-07 14:52:46 +0900

    week02 v0.3

--- week02_numpy.py
+++ week02_numpy.py
@@ -1,4 +1,6 @@
 import numpy as np
 
-v = np.array([1, 3, -9, 2])
-print(v.ndim)
+# v = np.array([1, 3, -9, 2])
+# v = np.array([1, 3, -9, 2], dtype='int64')
+v = np.array([[1, 3, -9, 2],[71, 13, -22, 7]], dtype='int64')
+print(v.ndim, v.shape, v.data, v.dtype, v.strides) # ndim : 차원, shape : 배열, data : 메모리 주소, : dtype : 데이터 타입, strides : 메모리 간격
