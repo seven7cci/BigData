5a1f02747e9d43abdca71c934423e4be17229d98
seven7cci <seven7cci@naver.com>
2023-09-07 16:39:14 +0900

    week02 v0.7

--- week02_numpy.py
+++ week02_numpy.py
@@ -1,6 +1,8 @@
 import random
 import numpy as np
+import tkinter as tk # Build in GUI
 
+"""
 # v = np.array([1, 3, -9, 2])
 # v = np.array([1, 3, -9, 2], dtype='int64')
 v = np.array([[1, 3, -9, 2],[71, 13, -22, 7]], dtype='int64')
@@ -14,3 +16,31 @@ l = [random.randint(1, 100) for i in range(n)]
 v = np.array(l, dtype='int16')
 print(v)
 # print(v.ndim, v.shape, v.data, v.dtype, v.strides)
+"""
+
+def click_button():
+    n = int(en_number.get())
+    l = [random.randint(1, 100) for i in range(n)]
+    v = np.array(l, dtype='int16')
+    lbl_result.config(text=v)
+
+
+window = tk.Tk()
+window.title('numpy gui version')
+window.geometry('300x150')
+
+#creare widget
+lbl_result = tk.Label(text="random numpy array")
+en_number = tk.Entry()
+btn_click = tk.Button(text="Click me", command=click_button)
+
+# widget layout
+lbl_result.pack()
+en_number.pack(fill='x')
+btn_click.pack(fill='x')
+
+window.mainloop()
+# n = int(input("input number : "))
+# l = [random.randint(1, 100) for i in range(n)]
+# v = np.array(l, dtype='int16')
+# print(v)
