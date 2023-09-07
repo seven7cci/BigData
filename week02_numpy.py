6d98cbcc4920d3f5cf9fc2bc383aa99573e24d34
seven7cci <seven7cci@naver.com>
2023-09-07 16:13:00 +0900

    week02 v0.4

--- week02_numpy.py
+++ week02_numpy.py
@@ -1,6 +1,16 @@
+import random
 import numpy as np
 
 # v = np.array([1, 3, -9, 2])
 # v = np.array([1, 3, -9, 2], dtype='int64')
 v = np.array([[1, 3, -9, 2],[71, 13, -22, 7]], dtype='int64')
 print(v.ndim, v.shape, v.data, v.dtype, v.strides) # ndim : 차원, shape : 배열, data : 메모리 주소, : dtype : 데이터 타입, strides : 메모리 간격
+
+n = int(input("input number : "))
+l = list()
+for i in range(n):
+    l.append(random.randint(1, 100))
+
+v = np.array(l, dtype='int16')
+print(v)
+print(v.ndim, v.shape, v.data, v.dtype, v.strides)
