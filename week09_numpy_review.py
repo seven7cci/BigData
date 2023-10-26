3645e830a98b6dd4a3939df53309140eaa1f5207
seven7cci <seven7cci@naver.com>
2023-10-26 16:29:16 +0900

    week09 numpy review 4

--- week09_numpy_review.py
+++ week09_numpy_review.py
@@ -1,24 +1,6 @@
 import numpy as np
-
-data = np.array([
-    [1, 5, 9],
-    [2, np.nan, 10],
-    [np.nan, 7, 11],
-    [4, 8, np.nan]
-])
-print(data)
-
-means = np.nanmean(data, axis=0)
-# print(means)
-for i in range(data.shape[1]):
-    # print(i)
-    mask = np.isnan(data[:, i])  # True, False 값을 갖는 배열 생성
-    data[mask, i] = means[i]
-
-print(data)
-
-# data = np.array([
-#     [1, 2, np.nan, 4],
-#     [5, np.nan, 7, 8],
-#     [9, 10, 11, np.nan]
-# ])
+a1 = np.array([1, 2, 3])
+a2 = np.array([4, 5, 6])
+print(a1 * a2)
+a3 = np.dot(a1, a2)
+print(a3)
