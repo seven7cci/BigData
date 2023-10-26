879b1b7e94e5ee5e400fb7b933a1aec82eaba917
seven7cci <seven7cci@naver.com>
2023-10-26 15:13:14 +0900

    week09 numpy review 1

---
+++ week09_numpy_review.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+np_1d = np.arange(1, 20, 2)
+# np_1d = np.arange(1.0, 10.0, 2.0)
+np_1d = np_1d.reshape(5, 2)
+print(np_1d, type(np_1d))
+print(np_1d.ndim)  # n-dimension, rank
+print(np_1d.size)  # size
+print(np_1d.shape)  # shape
+
+# np_1d = np.array([2, 4, 6, 8])
+# np_2d = np.array([[2, 4], [6, 8]])
+# print(np_1d.ndim, np_2d.ndim)  # n-dimension, rank
+# print(np_1d.size, np_2d.size)  # size
+# print(np_1d.shape, np_2d.shape)  # shape
+
+# any_list = [1, "university", 2.71, (5, 7)]
+# converted_any_list = ["1", "university", "2.71", "(5, 7)"]
+# one_type_np_list = np.array(converted_any_list)
+# one_type_np_list = np.array(any_list)  # value error
+
+
+# for i in any_list:
+#     print(i)
+#
+#
+# for j in np_1d:
+#     print(j)
