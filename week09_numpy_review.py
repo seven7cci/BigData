be3a7b395751cb559d4c727cf98358f4f7b88932
seven7cci <seven7cci@naver.com>
2023-10-26 15:43:05 +0900

    week09 numpy review 3

--- week09_numpy_review.py
+++ week09_numpy_review.py
@@ -1,14 +1,28 @@
 import numpy as np
+import pandas as pd
 
-def info(x):
-    print(f"배열의 차원(rank)은 {x.ndim}입니다.")
-    print(f"배열의 shape은 {x.shape}입니다.")
-    print(f"배열의 원소의 개수는 {x.size}입니다.")
+df = pd.DataFrame({
+    'A': [1, 2, np.nan, 4],
+    'B': [5, np.nan, 7, 8],
+    'C': [9, 10, 11, np.nan]
+})
+
+print(df)
+
+# # 1
+# df.fillna(df.mean(), inplace=True)
+# print(df)
+
+# # 2
+# for col in df.columns:
+#     # print(col)
+#     df[col] = np.where(pd.isnull(df[col]), np.mean(df[col]), df[col])
+
+# 3
+from sklearn.impute import SimpleImputer  # 결측값 전용 처리 클래스 활용
+i = SimpleImputer(strategy='mean')  # mean, median, most frequent
+df = pd.DataFrame(i.fit_transform(df), columns=df.columns)
+print(df)
 
 
-np_1d = np.arange(1, 20, 2)
-print(np_1d)
-info(np_1d)
-np_1d = np_1d.reshape(1, 2, 5)
-print(np_1d)
-info(np_1d)
+print(df)
