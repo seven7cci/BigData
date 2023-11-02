5d5613410d34954129912e79061f8598d5df7534
seven7cci <seven7cci@naver.com>
2023-11-02 16:14:15 +0900

    week10 v3

--- week10_pandas_review.py
+++ week10_pandas_review.py
@@ -1,21 +1,18 @@
 import numpy as np
-a1 = np.arange(1, 6)
-a2 = np.array([6, 7, 8, 9, 10])
-a3 = np.array([
-    [3, 2, 1],
-    [-11, 0, 9]
-])
-print(a1 + a2)
-print(a1 - a2)
-print(a1 * a2)
-print(a1 / a2)
-print(np.dot(a1, a2))
-print(np.sum(a1) + np.sum(a2))
-print(np.prod(a1), np.prod(a2))
 
-# by axis
-print(a3.sum(axis=0))
-print(a3.sum(axis=1))
-print(a3.prod(axis=0))
-print(a3.prod(axis=1))
-print(a3.mean())
+a = np.arange(1, 7).reshape(2, 3)
+print(a)
+
+b = np.array([
+    [5, 20],
+    [11, -1],
+    [9, 3]
+])
+print(b)
+
+print()
+
+print(a.dot(b))
+print(np.dot(a, b))
+print(b.dot(a))
+print(np.dot(b, a))
