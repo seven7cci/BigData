f51a6f6134a24bd34e5bddf10436685b83479140
seven7cci <seven7cci@naver.com>
2023-11-09 14:26:26 +0900

    week11 v2

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -7,8 +7,7 @@ df1 = pd.DataFrame(
         "MAT": [100, 90, 85],
     }, index=[1, 2, 3]
 )
-
-print(df1)
+# print(df1)
 
 df2 = pd.DataFrame(
     [
@@ -18,5 +17,13 @@ df2 = pd.DataFrame(
     ]
     , index=[1, 2, 3], columns=["KOR", "ENG", "MAT"]
 )
+print(df2)
 
+df2 = (
+    pd.melt(df2).rename(columns={
+            "variable": "subject",
+            "value": "score"
+        }
+    ).query('score >= 90')
+)
 print(df2)
