806d63296fa285f2e4c0f9210b517c21d5ba99e6
seven7cci <seven7cci@naver.com>
2023-11-09 15:42:32 +0900

    week11 v6

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -10,12 +10,15 @@ df = pd.DataFrame(
 )
 print(df)
 
+# 국어 성적과 영어 성적이 둘 다 95점 이상인 행을 추출
+print(df.query('KOR >= 95 and ENG >= 95'))
+
 # 국어, 수학 컬럼을 추출
 # 조건은 국어 성적이 95점 이상인 경우
 df_copy = df.loc[df['KOR'] >= 95, ['KOR', 'MAT']]
 
-# 1번 학생의 수학성적(100) 출력
-print(df.at[1, 'MAT'])
-print(df.iat[0, 2])
+# 1번 학생의 수학 성적(100) 출력
+print(df.at[1, 'MAT'])  # value by label
+print(df.iat[0, 2])  # value by index
 
 print(df_copy)
