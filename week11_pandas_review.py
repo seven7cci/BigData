4447e456f237b18c5f6bad31a20ac4fb0e1aaec9
seven7cci <seven7cci@naver.com>
2023-11-09 16:10:32 +0900

    week11 v9

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -8,27 +8,17 @@ df = pd.DataFrame(
         [83, 96, 94],
     ], index=[1, 2, 3, 4], columns=["KOR", "ENG", "MAT"]
 )
+
+
+
+# def scale_score(n):
+#     return n + 1
+#
+#
+# print(df)
+# df = df.apply(scale_score)
+# print(df)
+
 print(df)
-
-print(df.mean())
-print(df.max())
-
-# 국어 성적과 영어 성적이 둘 다 95점 이상인 행을 추출
-print(df.query('KOR >= 95 and ENG >= 95'))
-
-# 국어, 수학 컬럼을 추출
-# 조건은 국어 성적이 95점 이상인 경우
-df_copy = df.loc[df['KOR'] >= 95, ['KOR', 'MAT']]
-print(df_copy)
-
-# 1번 학생의 수학 성적(100) 출력
-print(df.at[1, 'MAT'])  # value by label
-print(df.iat[0, 2])  # value by index
-
-
-def scale_score(n):
-    return n + 1
-
-
-df = df.apply(scale_score)
+df = df.apply(lambda n: n + 1)
 print(df)
