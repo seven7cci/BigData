98cea661a74df0945ae0f06eb886f23826fed981
seven7cci <seven7cci@naver.com>
2023-11-16 15:20:11 +0900

    week12 V3

--- week12_web03.py
+++ week12_web03.py
@@ -2,26 +2,20 @@ from bs4 import BeautifulSoup
 
 html = """
 <html>
-    <head>
-        <title>스크레이핑 실습</title>
-    </head>
-    <body>
-        <h1>대림대학교</h1>
-        <p>웹스크레이핑</p>
-        <p>파이썬, 판다스, 넘파이, 맷플롯립, GUI...</p>
-    </body>
+<head>
+<title>스크레이핑 실습</title>
+</head>
+<body>
+<h1 id="univ">대림대학교</h1>
+<p>웹스크레이핑</p>
+<p id="contents">파이썬, 판다스, 넘파이, 맷플롯립, GUI...</p>
+</body>
 </html>
 """
-
 soup = BeautifulSoup(html, 'html.parser')
-t = soup.html.head.title
-print(t)
-print(t.string)
+university = soup.find(id='univ')
+contents = soup.find(id='contents')
 
-h1 = soup.html.body.h1.string
-print(h1)
-
-p1 = soup.html.body.p.string
-p2 = p1.next_sibling.sibling
-print(p1)
-print(p2)
+print(university)
+print(contents.string)
+print(contents)
