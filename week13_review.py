4feebfe7a16ad51aa1aa5fa175ae93bf97c8a6b7
seven7cci <seven7cci@naver.com>
2023-11-23 16:14:56 +0900

    week13 V2

--- week13_review.py
+++ week13_review.py
@@ -1,10 +1,11 @@
-import numpy as np
+import seaborn as sns
+import pandas as pd
 
-a1 = np.ones(5)
-a2 = np.array([1, 1, 1, 1, 1])
-a3 = np.arange(5) * 0 + 1
-a4 = np.zeros(5) + 1
-a5 = np.linspace(1, 1, 5)
-a6 = np.full(5, 1)
+titanic = sns.load_dataset('titanic')
 
-print(a1, a2, a3, a4, a5, a6)
+print(titanic.isnull())
+
+# print(titanic.head())
+# print(titanic.describe())
+# print(titanic.info())
+# print(titanic.tail())
