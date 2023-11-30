1a480a70b3644ec03334a5298858eba842611ed1
seven7cci <seven7cci@naver.com>
2023-11-30 14:29:00 +0900

    week13 V3

--- week13_review.py
+++ week13_review.py
@@ -3,9 +3,17 @@ import pandas as pd
 
 titanic = sns.load_dataset('titanic')
 
-print(titanic.isnull())
+print(titanic.isnull().sum())
+print(titanic['age'].fillna(titanic['age'].median(), inplace=True))
+print(titanic.isnull().sum())
+
+# print(titanic['deck'].fillna('unknown', inplace=True))  # 범주 항목에 unknown이 없어서 에러
+titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')  # 카테고리 추가
+print(titanic['deck'].fillna('Unknown', inplace=True))
 
 # print(titanic.head())
 # print(titanic.describe())
 # print(titanic.info())
 # print(titanic.tail())
+
+print(titanic['deck'].tail(10))
