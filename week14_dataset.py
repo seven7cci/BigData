6fae4bf6c1c5da1ba012d340d3358c67f5818b83
seven7cci <seven7cci@naver.com>
2023-11-30 14:42:40 +0900

    week14 V2

--- week14_dataset.py
+++ week14_dataset.py
@@ -2,14 +2,11 @@ import seaborn as sns
 import pandas as pd
 
 titanic = sns.load_dataset('titanic')
-#print(titanic.describe())
-#print(titanic.head(10))
-#print(titanic.tail(10))
 
-# 결측치 전처리
-print(titanic['age'].fillna(titanic['age'].median(), inplace=True))
-titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')  # 카테고리 추가
-titanic['deck'].fillna('Unknown', inplace=True)
-titanic['embarked'].fillna('Unknown', inplace=True)
-titanic['embark_town'].fillna('Unknown', inplace=True)
-print(titanic.info())
+
+# 생존자 수와 사망자 수 구하기
+survived_human = titanic[titanic['survived'] == 1]['survived'].count()
+dead_human = titanic[titanic['survived'] == 0]['survived'].count()
+print(f"생존자 수 :", survived_human)
+print(f"사망자 수 :", dead_human)
+
