19dbdf568f46afc55c4003bdb0f8178f62cfd7a8
seven7cci <seven7cci@naver.com>
2023-11-30 15:47:08 +0900

    week14 V6

--- week14_dataset.py
+++ week14_dataset.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 
 titanic = sns.load_dataset('titanic')
 
@@ -28,3 +29,14 @@ print(pclass_survived)
 # 나이대별 생존자 수 구하기
 age_survived = titanic.groupby(pd.cut(titanic['age'], bins=list(range(0, 81, 10))))['survived'].sum()
 print(age_survived)
+
+# 생존자와 사망자의 나이 분포를 시각화
+survived_ages = titanic[titanic['survived'] == 1]['age'].dropna()
+dead_ages = titanic[titanic['survived'] == 0]['age'].dropna()
+
+plt.hist(survived_ages, bins=20, label='Survived', alpha=0.5)
+plt.hist(dead_ages, bins=20, label='Dead', alpha=0.5)
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.legend()
+plt.show()
